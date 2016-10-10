1f03547e95eb9c1146d0e9244afa45af68a476e8
carol77 <carolina-day@hotmail.com>
2016-10-11 01:42:42 +0200

    Test brancha

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-/*To moja zmiana, ot co!*/ 
+/*To moja zmiana, ot co! */ 
 package Main;
 
 import ec.Evolve;
